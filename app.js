2ec22c153cd56cb28d50f220ef634316ac6db044
Wallner, Simon <Simon.Wallner@de-gmbh.com>
2020-11-10 13:55:01 +0100

    yeet

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-'use strict';
-console.log('Here\'s a hidden message!');
-let today = new Date();
-let formatDate = today.toDateString();
-let selectElement = document.getElementById('date');
-selectElement.innerHTML = formatDate;
+class HelloWorld extends React.Component {
+    render() {
+      // Tells React what HTML code to render
+      return <h1>Hello World</h1>
+    }
+  }
+  
+  // Tells React to attach the HelloWorld component to the 'root' HTML div
+  ReactDOM.render(<HelloWorld />, document.getElementById("root"));
