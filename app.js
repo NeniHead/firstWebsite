f15e6767db16349c42a75abfa2874e003a0b765e
Wallner, Simon <Simon.Wallner@de-gmbh.com>
2020-11-10 13:27:11 +0100

    fixed app

--- app.js
+++ app.js
@@ -2,6 +2,5 @@
 console.log('Here\'s a hidden message!');
 let today = new Date();
 let formatDate = today.toDateString();
-//let selectElement = document.getElementById('date');
-let selectElement = document.getElementsByClassName("list");
+let selectElement = document.getElementById('date');
 selectElement.innerHTML = formatDate;
